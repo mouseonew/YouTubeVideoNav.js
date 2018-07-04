796984d45342e4d4622fcce5383d55b44cc5bb08
mouseonew <40813981+mouseonew@users.noreply.github.com>
2018-07-03 20:11:15 -0700

    mouseonew

--- YouTubeVideoNav.js
+++ YouTubeVideoNav.js
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2017 0n3tw0 https://github.com/0n3tw0
+Copyright (c) 2017 0n3tw0 https://github.com/mouseonew
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
